2250b86641424f205acb671104ce6bb72525c7e2
lucasSPro <lucas.san.77@gmail.com>
2020-07-31 00:28:24 -0300

    receiving message with video from youtube

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const telegramBot = require('node-telegram-bot-api');
 const configs = require("./configs.json");
 const dialogFlow =  require('./dialogFlow');
+const youtube = require('./youtube-lib');
 
 const token = configs.Bot.key;
 
@@ -12,5 +13,10 @@ bot.on('message', async function (message) {
   console.log(message.text);
   const ReceivedResponse = await dialogFlow.sendMessage(chatID.toString(), message.text)
   
-  bot.sendMessage(chatID, ReceivedResponse.text);
+  let responseText = ReceivedResponse.text;
+
+  if(ReceivedResponse.intent === 'Exercício específico'){
+    responseText = await youtube.searchVideoUrl(responseText, ReceivedResponse.fields.corpo.stringValue)
+  }
+  bot.sendMessage(chatID, responseText);
 } )
--- youtube-lib.js
+++ youtube-lib.js
@@ -4,10 +4,20 @@ const config = require("./configs.json");
 const youtube =  new Youtube();
 youtube.setKey(config.youtube.keyYoutube);
 
-youtube.search("Exercicios en casa para abdome", 2, function(error, result){
-  if(!error){
-    console.log(JSON.stringify(result, null, 2));
-  }else{
-    console.log("deu erro");
-  }
-})
+function searchVideoUrl (message, queryText ) {
+  return new Promise((resolve, reject) => {
+    console.log(queryText);
+    youtube.search(`Exercicios em casa para ${queryText} `, 2, function(error, result){
+      if(!error){
+        const videoIds = result.items.map((item) => item.id.videoId).filter(item => item);
+        const youtubeLinks = videoIds.map( videoId => `https://www.youtube.com/watch?v=${videoId}`)
+        resolve(`${message} ${youtubeLinks.join(`, `)}`)
+        
+      }else{
+        reject('Deu erro');
+      }
+    })
+  })
+}
+
+module.exports.searchVideoUrl = searchVideoUrl
